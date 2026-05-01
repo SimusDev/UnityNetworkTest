8d9bf77c2588eb55717f4467019ff4b1f2c4175b
SimusDev <simusdev@mail.ru>
2026-05-01 22:46:02 +0300

    Health

---
+++ Assets/Scripts/Entity/Health.cs
@@ -0,0 +1,53 @@
+using PurrNet;
+using UnityEngine;
+using System;
+
+namespace Entity
+{
+    public class Health : MonoBehaviour
+    {
+        private SyncVar<float> _value = new SyncVar<float>();
+
+        public Action<float> ValueChanged;
+
+        private void Awake()
+        {
+            _value.onChanged += _ValueChanged;
+        }
+
+        private void OnDestroy()
+        {
+            _value.onChanged -= _ValueChanged;
+        }
+
+        private void _ValueChanged(float value)
+        {
+            ValueChanged?.Invoke(value);
+        }
+
+        public Health SetValue(float value)
+        {
+            _value.value = value;
+            return this;
+        }
+
+        public Health ApplyDamage(float damage)
+        {
+            _value.value -= damage;
+            return this;
+        }
+
+        public Health Heal(float points)
+        {
+            _value.value += points;
+            return this;
+        }
+
+        public float GetValue()
+        {
+            return _value.value;
+        }
+
+    }
+
+}
